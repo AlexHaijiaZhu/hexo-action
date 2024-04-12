e50a87b2a60dec78b757f3c8ecd6e6a183e8ebf7
Alex Zhu <hjzhu@uvic.ca>
2024-04-12 15:36:05 -0700

    Update entrypoint.sh
    fix conflict with hexo-douban

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+###
+ # @Author: Alex Zhu hjzhu@uvic.ca
+ # @Date: 2024-04-12 15:32:58
+ # @LastEditors: Alex Zhu hjzhu@uvic.ca
+ # @LastEditTime: 2024-04-12 15:34:47
+ # @FilePath: \Ferryd:\Documents\GitHub\hexo-action\entrypoint.sh
+ # @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+### 
 
 set -e
 
@@ -19,15 +27,15 @@ npm install hexo-deployer-git --save
 # deployment
 if [ "$INPUT_COMMIT_MSG" = "none" ]
 then
-    hexo g -d
+    hexo g --deploy
 elif [ "$INPUT_COMMIT_MSG" = "" ] || [ "$INPUT_COMMIT_MSG" = "default" ]
 then
     # pull original publish repo
     NODE_PATH=$NODE_PATH:$(pwd)/node_modules node /sync_deploy_history.js
-    hexo g -d
+    hexo g --deploy
 else
     NODE_PATH=$NODE_PATH:$(pwd)/node_modules node /sync_deploy_history.js
-    hexo g -d -m "$INPUT_COMMIT_MSG"
+    hexo g --deploy -m "$INPUT_COMMIT_MSG"
 fi
 
 echo ::set-output name=notify::"Deploy complate."
