80e891219c39c3159cf44153b74c1955b6ac95aa
Alex Zhu <hjzhu@uvic.ca>
2024-04-15 11:31:40 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,4 @@
 #!/bin/sh
-###
- # @Author: Alex Zhu hjzhu@uvic.ca
- # @Date: 2024-04-12 15:32:58
- # @LastEditors: Alex Zhu hjzhu@uvic.ca
- # @LastEditTime: 2024-04-12 15:34:47
- # @FilePath: \Ferryd:\Documents\GitHub\hexo-action\entrypoint.sh
- # @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
-### 
 
 set -e
 
